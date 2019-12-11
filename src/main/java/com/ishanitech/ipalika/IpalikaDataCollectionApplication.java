14a7da18733418e5bdc5d41db2caf0b2d6349658
Umesh Bhujel <yoomesbhujel@gmail.com>
2019-12-11 12:51:50 +0545

    Initialized the project repository.

---
+++ src/main/java/com/ishanitech/ipalika/IpalikaDataCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.ishanitech.ipalika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpalikaDataCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpalikaDataCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishanitech/ipalika/IpalikaDataCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishanitech.ipalika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpalikaDataCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

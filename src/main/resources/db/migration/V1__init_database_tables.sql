1889d6a895bec1f89d3cc3ada8c887f69e17f89a
Umesh Bhujel <yoomesbhujel@gmail.com>
2019-12-17 10:48:23 +0545

    Updated database schema

--- src/main/resources/db/migration/V1__init_database_tables.sql
+++ src/main/resources/db/migration/V1__init_database_tables.sql
@@ -1,19 +1,28 @@
 CREATE TABLE IF NOT EXISTS `user` (
-    `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `first_name` VARCHAR(30) NOT NULL,
-    `middle_name` VARCHAR(30),
-    `last_name` VARCHAR(30) NOT NULL,
-    `username` VARCHAR(30) NOT NULL UNIQUE,
-    `email` VARCHAR(50) UNIQUE KEY,
-    `password` VARCHAR(60) NOT NULL,
-    `phone_no` VARCHAR(10) NOT NULL,
-    `locked` BIT(1) NOT NULL DEFAULT 1,
-    `first_login` BIT(1) NOT NULL DEFAULT 1,
-    `enabled` BIT(1) NOT NULL DEFAULT 1,
-    `expired` BIT(1) NOT NULL DEFAULT 1,
-    `role_id` INT NOT NULL DEFAULT 1,
-    `reg_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`first_name` VARCHAR(30) NOT NULL,
+	`middle_name` VARCHAR(30) NULL DEFAULT NULL,
+	`last_name` VARCHAR(30) NOT NULL,
+	`username` VARCHAR(30) NOT NULL,
+	`email` VARCHAR(50) NULL DEFAULT NULL,
+	`password` VARCHAR(60) NOT NULL,
+	`mobile_number` VARCHAR(10) NOT NULL,
+	`locked` BIT(1) NOT NULL DEFAULT b'1',
+	`first_login` BIT(1) NULL DEFAULT b'1',
+	`enabled` BIT(1) NOT NULL DEFAULT b'1',
+	`expired` BIT(1) NOT NULL DEFAULT b'1',
+	`registered_date` TIMESTAMP NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+	PRIMARY KEY (`id`),
+	UNIQUE INDEX `username` (`username`),
+	UNIQUE INDEX `email` (`email`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=2;
+
+INSERT INTO `user` (`id`, `first_name`, `middle_name`, `last_name`, `username`, `email`, `password`, `mobile_number`, `locked`, `first_login`, `enabled`, `expired`, `registered_date`)
+ VALUES
+(1, 'Umesh', 'B.', 'Bhujel', 'yoomes', 'yoomesbhujel@gmail.com', '$2a$11$H9wDLxAPTX5qp0doFKank.w6vgB7xPo1CJojH2AC0ovBY4Iu31oTS', '9849931288', 0, 0, 1, 0, CURRENT_TIMESTAMP());
 
 CREATE TABLE IF NOT EXISTS `role` (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -29,3 +38,6 @@ CREATE TABLE IF NOT EXISTS `user_role` (
 	`role_id` INT(11) NOT NULL,
 	PRIMARY KEY (`user_id`, `role_id`)
 );
+
+INSERT INTO `user_role`(`user_id`, `role_id`) VALUES (1, 1),
+(1, 2);

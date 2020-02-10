302e5f028ca56b1f047c66614c6996b7f346fa1b
Umesh Bhujel <yoomesbhujel@gmail.com>
2020-02-10 16:57:05 +0545

    User remaining crud operations are added with extra security restrictions

--- src/main/java/com/ishanitech/ipalika/config/SecurityConfig.java
+++ src/main/java/com/ishanitech/ipalika/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -27,6 +28,7 @@ import com.ishanitech.ipalika.utils.JsonTokenHelper;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private CustomAuthenticationProvider authenticationProvider;
 	private CustomAuthenticationEntryPoint authenticationEntryPoint;
---
+++ src/main/java/com/ishanitech/ipalika/controller/UserController.java
@@ -0,0 +1,72 @@
+package com.ishanitech.ipalika.controller;
+
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ishanitech.ipalika.dto.UserDTO;
+import com.ishanitech.ipalika.exception.CustomSqlException;
+import com.ishanitech.ipalika.model.User;
+import com.ishanitech.ipalika.security.CustomUserDetails;
+import com.ishanitech.ipalika.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping("/user")
+@RestController
+public class UserController {
+	
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@Secured({"ROLE_SUPER_ADMIN", "ROLE_CENTRAL_ADMIN"})
+	@ResponseStatus(HttpStatus.CREATED)
+	@PostMapping
+	public void createUser(@RequestBody UserDTO user) throws CustomSqlException {
+		userService.addUser(user);
+	}
+	
+	//@PreAuthorize("hasRole('ROLE_SUPER_ADMIN') OR hasRole('ROLE_CENTRAL_ADMIN')")
+	@Secured({"ROLE_SUPER_ADMIN", "ROLE_CENTRAL_ADMIN"})
+	@DeleteMapping("/{userId}")
+	public void deleteUser(@PathVariable("userId") int userId, @AuthenticationPrincipal CustomUserDetails user) {
+		userService.deleteUser(userId);
+	}
+	
+	@Secured({"ROLE_SUPER_ADMIN", "ROLE_CENTRAL_ADMIN"})
+	@PostMapping("/{userId}")
+	public void disableUser(@PathVariable("userId") int userId) {
+		userService.disableUser(userId);
+	}
+	
+	
+	@PatchMapping(value = "/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void updateUserInfo(@PathVariable("userId") int userId,
+			@RequestBody Map<String, Object> updates) {
+		userService.updateUserInfoByUserId(updates, userId);
+	}
+	
+	@ResponseStatus(HttpStatus.OK)
+	@PutMapping("/{userId}/passoword")
+	public void changePassword(@RequestBody String password, @PathVariable("userId") int userId) {
+		userService.changePassword(password, userId);
+	}
+	
+}

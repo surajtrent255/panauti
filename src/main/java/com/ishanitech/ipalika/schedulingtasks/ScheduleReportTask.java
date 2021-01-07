7f15975366d2a745d01648ec0baca326e2614e51
Tanchhowpa Singzango <rev.x17@gmail.com>
2021-01-08 00:01:58 +0545

    added scheduler for all wards

--- src/main/java/com/ishanitech/ipalika/schedulingtasks/ScheduleReportTask.java
+++ src/main/java/com/ishanitech/ipalika/schedulingtasks/ScheduleReportTask.java
@@ -1,9 +1,12 @@
 package com.ishanitech.ipalika.schedulingtasks;
 
+import java.util.List;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.ishanitech.ipalika.service.ReportService;
+import com.ishanitech.ipalika.servicer.WardService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,9 +14,11 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class ScheduleReportTask {
 	private final ReportService reportService;
+	private final WardService wardService;
 	
-	public ScheduleReportTask(ReportService reportService) {
+	public ScheduleReportTask(ReportService reportService, WardService wardService) {
 		this.reportService = reportService;
+		this.wardService = wardService;
 	}
 
 	//Run Report Generation Service Everyday at midnight
@@ -22,5 +27,8 @@ public class ScheduleReportTask {
 	public void generateReport() {
 		log.info("Generate Report Called");
 		reportService.generateReport(0);
+		
+		List<Integer> wardNos = wardService.getAllWardNumbers();
+		wardNos.stream().forEach((wardNo) -> reportService.generateReport(wardNo));
 	}
 }
